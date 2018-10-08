cdbde8c5f4d2d97bc4577fba857c0855fab75ddd
danielv2 <daniel-v2@hotmail.com>
2018-10-08 14:52:43 -0300

    test express

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-var http = require('http'),
+var express = require('express'),
+app = express();
     fileSystem = require('fs'),
     path = require('path');
 
-http.createServer(function(request, response) {
-    var filePath = path.join(__dirname, 'myfile.mp3');
-    var stat = fileSystem.statSync(filePath);
-
-    response.writeHead(200, {
-        'Content-Type': 'audio/mpeg',
-        'Content-Length': stat.size
-    });
-
-    var readStream = fileSystem.createReadStream(filePath);
-    // We replaced all the event handlers with a simple call to readStream.pipe()
-    readStream.pipe(response);
-})
-.listen(80);
+app.get('/', function(req, res){
+  res.send('Ola Mundo');
+});
+app.listen(3000);
